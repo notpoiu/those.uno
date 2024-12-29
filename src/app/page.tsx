d605191bb88f731691b0dd83bcaf55e00885acf3
upio <75510171+notpoiu@users.noreply.github.com>
2024-12-29 15:15:26 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    setTimeout(() => {
+    setInterval(() => {
       router.push("https://github.com/notpoiu");
     }, 5000);
   }, []);
